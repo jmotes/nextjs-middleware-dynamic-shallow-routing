77f967de93123aeaf775928efebca8adb7d8dfd8
Jonathan Motes <jmotes@lapero.io>
2022-04-02 17:04:03 -0500

    Possible solution

--- pages/_sites/[site]/[...slug].tsx
+++ pages/_sites/[site]/[...slug].tsx
@@ -3,14 +3,6 @@ import { useRouter } from "next/router";
 import { ChangeEvent, useState } from "react";
 import Layout from "../../../components/Layout";
 
-const searchStateToURL = (searchState: any) => {
-  return searchState
-    ? `${window.location.pathname}?${new URLSearchParams(
-        searchState
-      ).toString()}`
-    : "";
-};
-
 const InnerPage: NextPage = () => {
   const [searchState, setSearchState] = useState<{ [key: string]: string }>();
   const router = useRouter();
@@ -18,11 +10,10 @@ const InnerPage: NextPage = () => {
   // push searchState changes to the querystring
   const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const newSearchState = { [e.target.name]: e.target.value };
-    const href = searchStateToURL(newSearchState);
-    router.push(href, undefined, {
-      shallow: true,
+    router.push({
+      pathname: window.location.pathname,
+      query: new URLSearchParams(newSearchState).toString(),
     });
-
     setSearchState(newSearchState);
   };
 
